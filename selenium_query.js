0d84e42263d80ab65a3b1a4299e17a0f6428a38d
Enrico Cervato <e.cervato@comparegroup.eu>
2013-10-30 20:19:12 +0100

    Moving back to selenium_query.js

--- selenium_query.js
+++ selenium_query.js
@@ -1,34 +1,58 @@
 var mysql = require('mysql')
 	,async = require('async')
 	,fs = require('fs')
+	,config = require('./config.json')
 
 var connection = mysql.createConnection({
-	  	host     : "localhost",
-	  	user     : "root",
-	  	password : "root"
+	  	host     : config.connection.host,
+	  	user     : config.connection.user,
+	  	password : config.connection.password
 	})
 
 connection.connect
 
-var fd = fs.openSync("out.txt", 'a+', 0666);  
+var fd = fs.openSync(config.fileName, 'a+', 0666);  
 
-connection.query('SELECT * FROM pcg_nlNL.category', function(err, rows, fields) {
+var categoriesArray = [];
+
+var execute = function () {
+	connection.query('SELECT * FROM pcg_nlNL.category where categoryId between 2406 and 2414 order by categoryId', function(err, rows, fields) {
+
 	 	if (err) throw err
+
   		fs.writeSync(fd, "first line");
 
 		async.forEach(rows,
-			function(item, done) {				
-				var categoryString = "item.categoryId=" + item.categoryId;
-				console.log(categoryString)
-				fs.writeSync(fd, categoryString);
+			function(category, done) {				
+				var categoryUrl =  config.baseUrl + "/" + category.urlFriendlyCategoryName
+
+				categoriesArray[category.categoryId] = category
+
+				console.log(categoryUrl)
+
+				fs.writeSync(fd, categoryUrl + '\n')
+
 				done()
 			}, 
 			function(err) {
 		    	console.log("finished");
-		    	fs.writeSync(fd, "last line");
+
 		    	fs.closeSync(fd);
+
+		    	console.log("categoriesArray.length=" + categoriesArray.length)
+
+				categoriesArray.forEach( function (item) {
+						console.log("item=" + item.categoryId)						
+					}
+				)
+
 		    			    	
 		});
 
 	});
+}
+
+execute();
+
+
 
