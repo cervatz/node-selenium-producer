9972ef2f6bf8b929adb970cea44a0d4b8a350240
Enrico Cervato <e.cervato@comparegroup.eu>
2013-10-31 11:50:55 +0100

    Small improvements.

--- selenium_query.js
+++ selenium_query.js
@@ -16,7 +16,7 @@ var categoriesArray = [];
 var loadCategories = function () {
 	console.log("loadCategories - entering ...")
 
-	connection.query("SELECT * FROM pcg_nlNL.category order by categoryId", function(err, rows, fields) {
+	connection.query("SELECT * FROM " + config.connection.schema + ".category order by categoryId", function(err, rows, fields) {
 
 	 	if (err) throw err
 
@@ -46,13 +46,15 @@ var writeCategories = function() {
 
 	var fd = fs.openSync(config.fileName, 'a+', 0666);  
 
+	fs.writeSync(fd, testHeader());
+
 	categoriesArray.forEach( function(category) {
 			console.log("category=" + category.categoryId)
 
 			if (category.type == 'rich' || category.type == 'carrental' || category.type == 'flights' || category.type == 'richlive' || category.type == 'crosslink') {
 				var categoryUrl =  config.baseUrl + "/" + category.urlFriendlyCategoryName
-				console.log("categoryUrl=" + categoryUrl);
-				fs.writeSync(fd, categoryUrl + '\n')				
+				console.log("categoryUrl=" + categoryUrl)
+				fs.writeSync(fd, testLine(categoryUrl))
 			}			
 		}
 	)
@@ -60,5 +62,42 @@ var writeCategories = function() {
 	fs.closeSync(fd);
 	console.log("writeCategories - leaving ...")
 }
+
+var testHeader = function() {
+	var header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+				+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
+				+ "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n"
+				+ "<head profile=\"http://selenium-ide.openqa.org/profiles/test-case\">\n"
+				+ "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"
+				+ "<link rel=\"selenium.base\" href=\"" + config.baseUrl +  "\" />\n"
+				+ "<title>Test</title>\n"
+				+ "</head>\n"
+				+ "<body>\n"
+				+ "<table cellpadding=\"1\" cellspacing=\"1\" border=\"1\">\n"
+				+ "<thead>\n"
+				+ "<tr><td rowspan=\"1\" colspan=\"3\">Test</td></tr>\n"
+				+ "</thead><tbody>\n"
+
+	return header;
+}
+
+var testFooter = function() {
+	var footer = "</tbody></table>\n"
+				+ "</body>\n"
+				+ "</html>\n"
+
+	return footer
+}
+
+var testLine = function(categoryUrl) {
+	var line = "<tr>\n"
+			+ "<td>open</td>\n"
+			+ "<td>" + categoryUrl + "</td>\n"
+			+ "<td></td>\n"
+			+ "</tr>\n";
+
+	return line;
+}
+
 
 loadCategories()
